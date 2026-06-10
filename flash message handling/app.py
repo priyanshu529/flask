7a3506064127a64212c7f2f0ec1d85d3d0dcdad2
priyanshu529 <pratiyushshishodiya@gmail.com>
2026-06-11 02:07:54 +0530

    updated

--- flash message handling/app.py
+++ flash message handling/app.py
@@ -3,17 +3,20 @@ from flask import Flask, render_template, request, flash,url_for, redirect
 app=Flask(__name__)
 app.secret_key="my-secret-key"
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def form():
     if request.method=="POST":
         name=request.form.get("name")
+        message=request.form.get("message")
         if not name:
             flash("Name is required!", "error")
             return redirect(url_for("form")) 
         flash(f"Thanks, {name}! your feedback was saved")
-        return render_template("thankyou.html")
+        return redirect(url_for("thankyou", name=name, message=message))
     return render_template("form.html")
 
 @app.route("/thankyou")
 def thankyou():
-    return render_template("thankyou.html")
+    name = request.args.get("name")
+    message = request.args.get("message")
+    return render_template("thankyou.html", name=name, message=message)
